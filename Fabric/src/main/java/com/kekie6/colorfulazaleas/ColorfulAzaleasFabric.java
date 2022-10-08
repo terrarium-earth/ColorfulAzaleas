3a15a86ac5dc0e15cebdfbcda6028a49e2a04783
Pug <batterypug@gmail.com>
2022-10-09 05:31:21 +1100

    Fix Halfdoors compat, add crafting recipe related content

--- Fabric/src/main/java/com/kekie6/colorfulazaleas/ColorfulAzaleasFabric.java
+++ Fabric/src/main/java/com/kekie6/colorfulazaleas/ColorfulAzaleasFabric.java
@@ -1,11 +1,20 @@
 package com.kekie6.colorfulazaleas;
 
+import amymialee.halfdoors.Halfdoors;
+import amymialee.halfdoors.blocks.HalfDoorBlock;
 import com.kekie6.colorfulazaleas.registry.AzaleaBlocks;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.registry.StrippableBlockRegistry;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
 
 public class ColorfulAzaleasFabric implements ModInitializer {
     public static final CreativeModeTab CREATIVE_TAB = FabricItemGroupBuilder.create(ColorfulAzaleas.id("colorful_azaleas")).icon(() -> new ItemStack(AzaleaBlocks.trees[5].sapling.get().asItem())).build();
@@ -18,6 +27,11 @@ public class ColorfulAzaleasFabric implements ModInitializer {
             AzaleaBlocks.ColorfulTree tree = AzaleaBlocks.trees[i];
             StrippableBlockRegistry.register(tree.woodType.log.get(), tree.woodType.stripped_log.get());
             StrippableBlockRegistry.register(tree.woodType.wood.get(), tree.woodType.stripped_wood.get());
+            if (FabricLoader.getInstance().isModLoaded("halfdoors")) {
+                ResourceLocation resourceLocation = ColorfulAzaleas.id(tree.woodType.name + "_azalea_halfdoor");
+                Block block = Registry.register(Registry.BLOCK, resourceLocation, new HalfDoorBlock(FabricBlockSettings.copyOf(Halfdoors.OAK_HALFDOOR)));
+                Registry.register(Registry.ITEM, resourceLocation, new BlockItem(block, new Item.Properties().tab(Halfdoors.DOOR_GROUP)));
+            }
         }
     }
 }
--- Fabric/src/main/java/com/kekie6/colorfulazaleas/platform/FabricPlatformHelper.java
+++ Fabric/src/main/java/com/kekie6/colorfulazaleas/platform/FabricPlatformHelper.java
@@ -1,27 +1,13 @@
 package com.kekie6.colorfulazaleas.platform;
 
-import amymialee.halfdoors.Halfdoors;
-import amymialee.halfdoors.blocks.HalfDoorBlock;
 import com.google.auto.service.AutoService;
-import com.kekie6.colorfulazaleas.ColorfulAzaleas;
 import com.kekie6.colorfulazaleas.ColorfulAzaleasFabric;
 import com.kekie6.colorfulazaleas.platform.services.IPlatformHelper;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.core.Holder;
-import net.minecraft.core.Registry;
-import net.minecraft.data.BuiltinRegistries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
@@ -58,13 +44,6 @@ public class FabricPlatformHelper implements IPlatformHelper {
                 tableBuilder.pool(poolBuilder1.build());
             }
         }));
-    }
-
-    @Override
-    public void registerHalfDoor(ResourceLocation resourceLocation) {
-        Block block = new HalfDoorBlock(FabricBlockSettings.copyOf(Halfdoors.OAK_HALFDOOR));
-        Registry.register(Registry.BLOCK, resourceLocation, block);
-        Registry.register(Registry.ITEM, resourceLocation, new BlockItem(block, new Item.Properties().tab(Halfdoors.DOOR_GROUP)));
     }
 
     @Override
